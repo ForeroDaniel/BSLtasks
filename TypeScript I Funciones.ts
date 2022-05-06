4ee59a778bb6d60f956e2e8fa132ce9c8a8a1ffd
Daniel Forero <99362093+SimplyOnEarth@users.noreply.github.com>
2022-05-05 21:12:01 -0500

    Update and rename ejercicio to TypeScript I Funciones.ts

--- TypeScript I Funciones.ts
+++ TypeScript I Funciones.ts
@@ -1,4 +1,13 @@
+//EJERCICIO 1
 const myPromise = Promise.resolve(1)
 myPromise.then((value) => {
   console.log(value) // log: 1
 }) 
+
+//EJERCICIO 2
+
+//EJERCICIO 3
+
+//EJERCICIO 4
+
+//EJERCICIO 5
