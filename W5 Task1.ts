af4f45ecaf003b4a7b3b7097e2dd5ff83862db94
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2022-05-17 17:17:32 -0500

    Create W5 Task1.ts

---
+++ W5 Task1.ts
@@ -0,0 +1,25 @@
+EJERCICIO 1
+
+export function quarterOf(month: number): number {
+  let result:number;
+  if (month > 0 && month < 4){
+    result = 1;
+  }else if (month > 3 && month < 7){
+    result = 2;
+  }else if (month > 6 && month < 10){
+    result = 3;
+  }else { result = 4; }
+  
+  return result
+}
+                                     
+                                     
+                                     
+EJERCICIO 2
+                                     
+EJERCICIO 3
+                                     
+EJERCICIO 4
+                                     
+EJERCICIO 5
+                                     
