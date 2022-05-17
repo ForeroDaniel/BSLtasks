1dcceeb750fbed62052d46b9eeef0816d0883a3d
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2022-05-17 17:35:50 -0500

    Update W5 Task1.ts

--- W5 Task1.ts
+++ W5 Task1.ts
@@ -25,7 +25,10 @@ export const boolToWord = (bool: boolean): string => {
 
 //EJERCICIO 3
   
-
+export function abbrevName(name: string): string {
+    const splitName = name.toUpperCase().split(' ');
+    return `${splitName[0][0]}.${splitName[1][0]}`;
+}
 
 
 
