8de8ec7d87d3080729cde8c6e0773afc44a813fc
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2022-05-18 08:29:06 -0500

    Update W5 Task2.ts

--- W5 Task2.ts
+++ W5 Task2.ts
@@ -1,4 +1,4 @@
-EJERCICIO 1
+//EJERCICIO 1
 
 export function gooseFilter (birds: string[]): string[] {
   const geese: [string] = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
@@ -7,5 +7,17 @@ export function gooseFilter (birds: string[]): string[] {
 
 
 
-EJERCICIO 2
+//EJERCICIO 2
+
+export class Kata {
+  static highAndLow(numbers: string): string {
+    const stringArray = numbers.split(' ');
+    const numberArray = stringArray.map((item) => parseInt(item));
+    return `${Math.max.apply(null, numberArray)} ${Math.min.apply(null, numberArray)}`; 
+  }
+}
+
+
+
+//EJERCICIO 3
 
