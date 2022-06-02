cef0d5fdacbb6c72a4c2df7dfae85ad5ebaa1192
Daniel Forero <99362093+ForeroDaniel@users.noreply.github.com>
2022-06-01 22:08:30 -0500

    Update W7 Task2.ts

--- W7 Task2.ts
+++ W7 Task2.ts
@@ -3,12 +3,28 @@ abstract class Character {
   constructor() {}
   
   introduce() {}
+  walk() {}
+  run() {}
+  abstract uniqueVoiceTone();
+  abstract usualVoicePace();
 }
 
 class Protagonist extends Character {
-  
+  constructor() {
+    super();
+  }
+  uniqueVoiceTone() {}
+  usualVoicePace() {}
+  fly() {}
+  sing() {}
 }
 
 class Antagonist extends Character {
-  
+  constructor() {
+    super();
+  }
+  uniqueVoiceTone() {}
+  usualVoicePace() {}
+  dance() {}
+  paint() {}
 }
